a9abc53dc76b4275eb03e6af726858d303db96bd
Lungile Nyakama <lungile.nyakama@gmail.com>
2020-09-12 14:50:30 +0200

    Completed the create team form.

--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -13,6 +13,7 @@ namespace TrackerLibrary.DataAccess
     {
         private const string PrizesFile = "PrizeModels.csv";
         private const string PeopleFile = "PersonModels.csv";
+        private const string TeamFile = "TeamModels.csv";
 
         public PersonModel CreatePerson(PersonModel model)
         {
@@ -62,6 +63,29 @@ namespace TrackerLibrary.DataAccess
 
             return model;           
            
+        }
+
+        public TeamModel CreateTeam(TeamModel model)
+        {
+            List<TeamModel> teams = TeamFile.FullFilePath().LoadFile().ConvertToTeamModels(PeopleFile);
+
+            // Find the max ID
+            int currentId = 1;
+
+            if (teams.Count > 0)
+            {
+                currentId = teams.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            // Add the new record with the new ID (max + 1)
+            teams.Add(model);
+
+            // Save the list<string> to the text file
+            teams.SaveToTeamFile(TeamFile);
+
+            return model;
         }
 
         public List<PersonModel> GetPerson_All()
--- TournamentTracker/TrackerLibrary/Models/TeamModel.cs
+++ TournamentTracker/TrackerLibrary/Models/TeamModel.cs
@@ -8,7 +8,18 @@ namespace TrackerLibrary.Models
 {
     public class TeamModel
     {
-        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        /// <summary>
+        /// The unique identifier for the team
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// The name of the team
+        /// </summary>
         public string TeamName { get; set; }
+        /// <summary>
+        /// The list of team members
+        /// </summary>
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        
     }
 }
